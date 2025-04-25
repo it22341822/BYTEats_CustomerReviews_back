f3791fba2d7f9d49828131ffabc0be351a223235
SandaliAma <amakonara16@gmail.com>
2025-04-25 20:54:39 +0530

    Initial commit

---
+++ config/db.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    const uri = process.env.MONGODB_URI; 
+    if (!uri) {
+      throw new Error('MongoDB URI is not defined');
+    }
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected');
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
